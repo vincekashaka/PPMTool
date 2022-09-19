ce43e802929260a8b4c9541c3fdf1b91df54a427
Vincent Kashaka <vincekashaka@gmail.com>
2022-09-19 21:30:20 +0200

    Custome Exceptons for Unique project Identifier

---
+++ PPMToolFullStack/src/main/java/com/vince/ppmtool/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,16 @@
+package com.vince.ppmtool.exceptions;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectIdExceptionResponse {
+
+    private String projectIdentifier;
+
+
+}
--- PPMToolFullStack/src/main/java/com/vince/ppmtool/service/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/vince/ppmtool/service/ProjectService.java
@@ -1,9 +1,12 @@
 package com.vince.ppmtool.service;
 
 import com.vince.ppmtool.domain.Project;
+import com.vince.ppmtool.exceptions.ProjectIdException;
 import com.vince.ppmtool.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 public class ProjectService {
@@ -11,8 +14,13 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+    public Project saveOrUpdateProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 
 }
