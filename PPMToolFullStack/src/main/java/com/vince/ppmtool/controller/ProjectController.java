ff57f26c12ccceca54fb6b5fe735889f4b4660f7
Vincent Kashaka <vincekashaka@gmail.com>
2022-09-20 07:00:56 +0200

    Delete existing Projects

--- PPMToolFullStack/src/main/java/com/vince/ppmtool/controller/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/vince/ppmtool/controller/ProjectController.java
@@ -45,4 +45,10 @@ public class ProjectController {
     public Iterable<Project> getAllProjects(){
         return projectService.findAllProjects();
     }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId){
+        projectService.deleteProjectByIdentifier(projectId);
+        return new ResponseEntity<String>("Project with Id '" +projectId+"' was deleted", HttpStatus.OK);
+    }
 }
--- PPMToolFullStack/src/main/java/com/vince/ppmtool/service/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/vince/ppmtool/service/ProjectService.java
@@ -35,4 +35,14 @@ public class ProjectService {
         return projectRepository.findAll();
     }
 
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if (project == null){
+            throw new ProjectIdException("Can not delete project with ID '" + projectId +"'. This project does not exist");
+        }
+
+        projectRepository.delete(project);
+    }
+
 }
