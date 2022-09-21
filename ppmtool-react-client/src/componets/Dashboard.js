cd01bd1544476b4e429fdf52d1d2d6bd041e8526
Vincent Kashaka <vincekashaka@gmail.com>
2022-09-21 11:26:48 +0200

    Styled Dashboard, ProjectItem and Navbar components

--- ppmtool-react-client/src/componets/Dashboard.js
+++ ppmtool-react-client/src/componets/Dashboard.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import ProjectItem from './project/ProjectItem';
-
-export default class Dashboard extends Component {
+class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1 className='alert alert-waring'> Welcome to the Dashboard</h1>;
-        <ProjectItem />
+      <div className='projects'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-12'>
+              <h1 className='display-4 text-center'>Projects</h1>
+              <br />
+              <a href='ProjectForm.html' className='btn btn-lg btn-info'>
+                Create a Project
+              </a>
+              <br />
+              <hr />
+              <ProjectItem />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+export default Dashboard;
