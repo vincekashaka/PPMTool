be7373f5b28774f7641c168c9bd1940f71514343
Vincent Kashaka <vincekashaka@gmail.com>
2022-09-26 13:19:37 +0200

    Style error validation

--- ppmtool-react-client/src/componets/project/AddProject.js
+++ ppmtool-react-client/src/componets/project/AddProject.js
@@ -65,14 +65,23 @@ class AddProject extends Component {
                   <div className='form-group'>
                     <input
                       type='text'
-                      className='form-control form-control-lg '
+                      className={`form-control form-control-lg ${
+                        errors.projectName ? 'is-invalid' : ''
+                      }`}
                       placeholder='Project Name'
                       name='projectName'
                       value={this.state.projectName}
                       onChange={this.onChange}
                     />
-                    <p>{errors.projectName}</p>
+
+                    {errors.projectName && (
+                      <div className='invalid-feedback'>
+                        {errors.projectName}
+                      </div>
+                    )}
+                    <br />
                   </div>
+
                   <div className='form-group'>
                     <input
                       type='text'
