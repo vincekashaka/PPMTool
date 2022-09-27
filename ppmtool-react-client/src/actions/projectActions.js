4a1addd53c62a3d2b43c37b857716a2b9274084d
Vincent Kashaka <vincekashaka@gmail.com>
2022-09-27 22:14:30 +0200

    Get project by id

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { GET_ERRORS, GET_PROJECT, GET_PROJECTS } from './types';
 
-import { GET_ERRORS, GET_PROJECTS } from './types';
-
-// const navigate = useNavigate();
+// const history = useNavigate();
 
 export const createProject = (project, history) => async (dispatch) => {
   try {
@@ -13,7 +13,8 @@ export const createProject = (project, history) => async (dispatch) => {
     );
 
     // dispatch({ type: GET_ERRORS, payload: { ...res, history } });
-    history.push('/');
+    // history.push('/');
+
     // navigate('/');
   } catch (err) {
     console.log(err.response);
@@ -28,6 +29,14 @@ export const getProjects = () => async (dispatch) => {
   const res = await axios.get('http://localhost:8080/api/v1/project/all');
   dispatch({
     type: GET_PROJECTS,
+    payload: res.data,
+  });
+};
+
+export const getProject = (id, history) => async (dispatch) => {
+  const res = await axios.get(`http://localhost:8080/api/v1/project/${id}`);
+  dispatch({
+    type: GET_PROJECT,
     payload: res.data,
   });
 };
--- ppmtool-react-client/src/componets/project/UpdateProject.js
+++ ppmtool-react-client/src/componets/project/UpdateProject.js
@@ -1,65 +1,139 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { PropTypes } from 'prop-types';
+import { connect, useDispatch } from 'react-redux';
+import { getProject } from '../../actions/projectActions';
+import { useNavigate, useParams } from 'react-router-dom';
 
-class UpdateProject extends Component {
-  render() {
-    return (
-      <div className='project'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-8 m-auto'>
-              <h5 className='display-4 text-center'>Update Project form</h5>
-              <hr />
-              <form>
-                <div className='form-group'>
-                  <input
-                    type='text'
-                    className='form-control form-control-lg '
-                    placeholder='Project Name'
-                  />
-                </div>
-                <br />
-                <div className='form-group'>
-                  <input
-                    type='text'
-                    className='form-control form-control-lg'
-                    placeholder='Unique Project ID'
-                    disabled
-                  />
-                </div>
-                <br />
-                <div className='form-group'>
-                  <textarea
-                    className='form-control form-control-lg'
-                    placeholder='Project Description'
-                  ></textarea>
-                </div>
-                <h6>Start Date</h6>
-                <div className='form-group'>
-                  <input
-                    type='date'
-                    className='form-control form-control-lg'
-                    name='start_date'
-                  />
-                </div>
-                <h6>Estimated End Date</h6>
-                <div className='form-group'>
-                  <input
-                    type='date'
-                    className='form-control form-control-lg'
-                    name='end_date'
-                  />
-                </div>
+const UpdateProject = ({ project }) => {
+  const [projectName, setprojectName] = useState(project.projectName);
+  const [projectIdentifier, setprojectIdentifier] = useState(
+    project.projectIdentifier
+  );
+  const [projectDescription, setprojectDescription] = useState(
+    project.projectDescription
+  );
+  const [startDate, setstartDate] = useState(project.startDate);
+  const [endDate, setendDate] = useState(project.endDate);
 
+  let { id } = useParams();
+
+  console.log(id);
+  // class UpdateProject extends Component {
+  //   componentDidMount() {
+  //     const { id } = this.props.match.params;
+  //     this.props.getProject(id, this.props.history);
+  //   }
+  useEffect(() => {
+    getProject(id);
+  }, []);
+
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(
+      getProject({
+        projectName,
+        projectIdentifier,
+        projectDescription,
+        startDate,
+        endDate,
+      })
+    );
+    setprojectName(project.projectName);
+
+    // if (
+    //   !projectName ||
+    //   !projectIdentifier ||
+    //   !projectDescription ||
+    //   !startDate ||
+    //   !endDate
+    // ) {
+    //   return alert('Please fill out all fields');
+    // }
+
+    getProject(
+      projectName,
+      projectIdentifier,
+      projectDescription,
+      startDate,
+      endDate
+    );
+  };
+  return (
+    <div className='project'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-8 m-auto'>
+            <h5 className='display-4 text-center'>Update Project form</h5>
+            <hr />
+            <form onSubmit={onSubmit}>
+              <div className='form-group'>
                 <input
-                  type='submit'
-                  className='btn btn-primary btn-block mt-4'
+                  type='text'
+                  className='form-control form-control-lg '
+                  name='projectName'
+                  value={projectName}
+                  onChange={(e) => setprojectName(e.target.value)}
                 />
-              </form>
-            </div>
+              </div>
+              <br />
+              <div className='form-group'>
+                <input
+                  type='text'
+                  className='form-control form-control-lg'
+                  placeholder='Unique Project ID'
+                  disabled
+                />
+              </div>
+              <br />
+              <div className='form-group'>
+                <textarea
+                  className='form-control form-control-lg'
+                  name='projectIdentifier'
+                  value={projectIdentifier}
+                  onChange={(e) => setprojectDescription(e.target.value)}
+                ></textarea>
+              </div>
+              <h6>Start Date</h6>
+              <div className='form-group'>
+                <input
+                  type='date'
+                  className='form-control form-control-lg'
+                  name='startDate'
+                  value={startDate}
+                  onChange={(e) => setstartDate(e.target.value)}
+                />
+              </div>
+              <h6>Estimated End Date</h6>
+              <div className='form-group'>
+                <input
+                  type='date'
+                  className='form-control form-control-lg'
+                  name='endDate'
+                  value={endDate}
+                  onChange={(e) => setendDate(e.target.value)}
+                />
+              </div>
+
+              <input type='submit' className='btn btn-primary btn-block mt-4' />
+            </form>
           </div>
         </div>
       </div>
-    );
-  }
-}
-export default UpdateProject;
+    </div>
+  );
+};
+
+UpdateProject.propTypes = {
+  getProject: PropTypes.func.isRequired,
+  project: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  project: state.project.project,
+});
+export default connect(mapStateToProps, { getProject })(UpdateProject);
