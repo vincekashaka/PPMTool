31a1b805516922b0154ecbd2d88fd376296db350
Vincent Kashaka <vincekashaka@gmail.com>
2022-09-27 10:48:02 +0200

    Update project Form

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './componets/Dashboard';
 import Header from './componets/layout/Header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddProject from './componets/project/AddProject';
+import UpdateProject from './componets/project/UpdateProject';
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Dashboard />} />
           <Route exact path='/addProject' element={<AddProject />} />
+          <Route exact path='/updateProject/:id' element={<UpdateProject />} />
+
           <Route />
         </Routes>
       </div>
